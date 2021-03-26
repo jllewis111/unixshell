ec31dcd25ded77993d15873dcc6b483b9fce9085
Jerry Liu <47126890+jllewis111@users.noreply.github.com>
2021-03-25 22:49:24 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/types.h>
+//#include <sys/wait.h>
+
+
+
+
+
+
+char** parse(char* s) {
+  static char* words[500];
+  memset(words, 0, sizeof(words));
+  
+  char break_chars[] = " \t";
+
+  int i = 0;
+  char* p = strtok(s, break_chars);
+  words[i++] = p;
+  while (p != NULL) {
+    p = strtok(NULL, break_chars);
+    words[i++] = p;
+  }
+  return words;
+}
+
+
+int main(int argc, const char * argv[]) {  
+  char input[BUFSIZ];
+  char last_command[BUFSIZ];
+  
+  memset(input, 0, BUFSIZ * sizeof(char));
+  memset(input, 0, BUFSIZ * sizeof(char));
+  bool finished = false;
+  
+  while (!finished) {
+    printf("osh> ");
+    fflush(stdout);
+
+    if ((fgets(input, BUFSIZ, stdin)) == NULL) {   // or gets(input, BUFSIZ);
+      fprintf(stderr, "no command entered\n");
+      exit(1);
+    }
+    input[strlen(input) - 1] = '\0';          // wipe out newline at end of string
+    printf("input was: \n'%s'\n", input);
+
+    // check for history (!!) command
+    if (strncmp(input, "!!", 2) == 0) {
+      if (strlen(last_command) == 0) {
+        fprintf(stderr, "no last command to execute\n");
+      }
+      printf("last command was: %s\n", last_command);
+    } else if (strncmp(input, "exit", 4) == 0) {   // only compare first 4 letters
+      finished = true;
+    } else {
+      strcpy(last_command, input);
+      printf("You entered: %s\n", input);   // you will call fork/exec
+      parse(input);
+			printf("after parse, what is input: %s\n", input);
+      printf("\n");
+      pid_t pid = fork();
+
+        if (pid == 0) {
+                printf("Child.\n");
+        }
+        if (pid > 0) {
+                printf("I am the parent, the child is %d.\n", pid);
+        }
+        if (pid < 0) {
+                perror("In fork():");
+        }
+
+        exit(0);
+    }
+  }
+  
+
+  printf("Exited\n");
+  
+  return 0;
+}
